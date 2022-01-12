efcf94c27774bada9fb90f7f31b0f40a6070a11f
Joshua Oreman <oremanj@hudson-trading.com>
2022-01-11 21:31:30 -0700

    Fix package name

--- ci.sh
+++ ci.sh
@@ -3,7 +3,7 @@
 set -ex -o pipefail
 
 pip install -U pip setuptools wheel
-sudo apt-get install libnetfilterqueue-dev
+sudo apt-get install libnetfilter-queue-dev
 python setup.py sdist --formats=zip
 pip install dist/*.zip
 pip install -r test-requirements.txt
